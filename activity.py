8418b31708a6dbecd14e75ff12738fa3db77442b
James Cameron <quozl@laptop.org>
2018-07-26 11:22:53 +1000

    Port to GTK+ 3

--- activity.py
+++ activity.py
@@ -1,9 +1,12 @@
 import subprocess
 
-import glib
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
 
-from sugar.activity.activity import Activity
+from gi.repository import GLib
+from gi.repository import Gtk
+
+from sugar3.activity.activity import Activity
 
 
 class GComprisLauncher(Activity):
@@ -11,16 +14,17 @@ class GComprisLauncher(Activity):
     def __init__(self, handle):
         # Initialize the parent
         Activity.__init__(self, handle)
-        hbox = gtk.HBox()
+
+        self.max_participants = 1
+
+        hbox = Gtk.HBox()
         self.set_canvas(hbox)
         self.show_all()
-        # options = ['gcompris', '--nolockfile', '--native', '--fullscreen', '--noprint']
         options = ['gcompris', '--fullscreen']
         proc = subprocess.Popen(options)
 
         # Stay alive with a blank window mapped for at least 60 seconds
         # so that the shell knows that we launched
-        glib.timeout_add_seconds(60, gtk.main_quit)
+        GLib.timeout_add_seconds(60, Gtk.main_quit)
         # but get rid of that window if the child exits beforehand
-        glib.child_watch_add(proc.pid, gtk.main_quit)
-
+        GLib.child_watch_add(proc.pid, Gtk.main_quit)
